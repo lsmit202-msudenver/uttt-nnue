fcba5941e2d4891ddd606d752c53c6cef83c1b64
Luke Smith <lsmit202@msudenver.edu>
2025-01-29 19:11:31 +0000

    project builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
 
 conan install .. --output-folder=. --build=missing
-cmake -DBUILD_TESTING=TRUE -DBUILD_SHARED_LIBS=TRUE -DCMAKE_BUILD_TYPE=Release ..
+cmake -DBUILD_TESTING=TRUE -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
 
 if command -v clang-format &> /dev/null; then
